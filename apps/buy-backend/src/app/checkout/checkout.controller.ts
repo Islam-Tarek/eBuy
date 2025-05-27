23f87be3bae677466e69ac160754263518f3646c
Islam-Tarek <islamtarek258@gmail.com>
2025-05-27 05:34:25 +0300

    feat add checkout component

---
+++ apps/buy-backend/src/app/checkout/checkout.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Post, Body, HttpException, Headers } from '@nestjs/common';
+import { CheckoutService } from './checkout.service';
+import { CreateCheckoutDto } from './dto/create-checkout.dto';
+
+@Controller('checkout')
+export class CheckoutController {
+  constructor(private readonly checkoutService: CheckoutService) {}
+
+  @Post()
+  async create(
+    @Body() createCheckoutDto: CreateCheckoutDto,
+    @Headers('authorization') authHeader: string
+  ) {
+    const token = authHeader ? authHeader.substring(7) : '';
+    const session = await this.checkoutService.create(createCheckoutDto, token);
+    if (!session.url) {
+      throw new HttpException('Could not create a checkout session', 400);
+    }
+
+    return {
+      url: session.url,
+    };
+  }
+}
